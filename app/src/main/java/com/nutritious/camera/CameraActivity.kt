66728ca4e60d00c6788835df28a85fe19fe49611
Jakub Domagala <domagala.jacob@gmail.com>
2019-11-24 02:19:55 +0100

    Change icons and add button

--- app/src/main/java/com/nutritious/camera/CameraActivity.kt
+++ app/src/main/java/com/nutritious/camera/CameraActivity.kt
@@ -17,6 +17,7 @@
 package com.nutritious.camera
 
 import android.os.Bundle
+import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
 import com.nutritious.R
 
@@ -24,6 +25,7 @@ class CameraActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_camera)
         savedInstanceState ?: supportFragmentManager.beginTransaction()
                 .replace(R.id.container, Camera2BasicFragment.newInstance())
